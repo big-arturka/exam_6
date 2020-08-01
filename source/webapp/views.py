bd7e8f65637153e6223e7d7d437699600732273f
Arthur <arturkrmnlv10@gmail.com>
2020-08-01 12:30:56 +0600

    Добавлено представление добавления нового отзыва, отредактирован html шаблони и форма.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from webapp.models import Entry, ENTRY_STATUS
 from django.http import HttpResponseNotAllowed
+from webapp.forms import EntryForm
 
 
 def index_view(request):
@@ -8,12 +9,19 @@ def index_view(request):
     return render(request, 'index.html', context={'entry': entry})
 
 
-def entry_view(request, pk):
-    pass
-
-
 def entry_create(request):
-    pass
+    if request.method == 'GET':
+        return render(request, 'entry_create.html',
+                      context={'form': EntryForm()})
+    elif request.method == 'POST':
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            Entry.objects.create(**form.cleaned_data)
+            return redirect('index')
+        else:
+            return render(request, 'entry_create.html', context={'form': form})
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
 
 
 def entry_update(request, pk):
