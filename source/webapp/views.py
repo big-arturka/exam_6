bc771c91edd8add29bf6962928dca8a690e8bc7c
Arthur <arturkrmnlv10@gmail.com>
2020-08-01 15:11:11 +0600

    Форма создания нового отзыва перенесена на главную страницу. Отредактированы html файлы и представления. Удалена отдельная страница создания отзыва.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -8,20 +8,24 @@ def index_view(request):
     entry = Entry.objects.filter(status='active').order_by('-created_at')
     if request.GET.get('search_item'):
         entry = Entry.objects.filter(status='active', author=request.GET.get('search_item')).order_by('-created_at')
-    return render(request, 'index.html', context={'entry': entry})
+    return render(request, 'index.html', context={'entry': entry,
+                                                  'form': EntryForm()})
 
 
 def entry_create(request):
+    entry = Entry.objects.filter(status='active').order_by('-created_at')
     if request.method == 'GET':
-        return render(request, 'entry_create.html',
-                      context={'form': EntryForm()})
+        if request.GET.get('search_item'):
+            entry = Entry.objects.filter(status='active', author=request.GET.get('search_item')).order_by('-created_at')
+        return render(request, 'index.html', context={'entry': entry,
+                                                      'form': EntryForm()})
     elif request.method == 'POST':
         form = EntryForm(data=request.POST)
         if form.is_valid():
             Entry.objects.create(**form.cleaned_data)
             return redirect('index')
         else:
-            return render(request, 'entry_create.html', context={'form': form})
+            return render(request, 'index.html', context={'form': form})
     else:
         return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
 
@@ -40,7 +44,6 @@ def entry_update(request, pk):
             entry.author = form.cleaned_data['author']
             entry.email = form.cleaned_data['email']
             entry.description = form.cleaned_data['description']
-            entry.status = form.cleaned_data['status']
             entry.save()
             return redirect('index')
         else:
