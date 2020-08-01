25363838785361c039001c3d546c2e69ba592965
Arthur <arturkrmnlv10@gmail.com>
2020-08-01 12:37:50 +0600

    Добавлено представление обновления отзыва, отредактирован html шаблони и форма.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -25,7 +25,28 @@ def entry_create(request):
 
 
 def entry_update(request, pk):
-    pass
+    entry = get_object_or_404(Entry, pk=pk)
+    if request.method == 'GET':
+        form = EntryForm(initial={'author': entry.author,
+                                  'email': entry.email,
+                                  'description': entry.description,
+                                  'status': entry.status})
+        return render(request, 'entry_update.html', context={'form': form, 'entry': entry})
+    elif request.method == 'POST':
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            entry.author = form.cleaned_data['author']
+            entry.email = form.cleaned_data['email']
+            entry.description = form.cleaned_data['description']
+            entry.status = form.cleaned_data['status']
+            entry.save()
+            return redirect('index')
+        else:
+            return render(request, 'entry_update.html',
+                          context={'form': form,
+                                   'entry': entry})
+    else:
+        return HttpResponseNotAllowed(permitted_methods=['GET', 'POST'])
 
 
 def entry_delete(request, pk):
