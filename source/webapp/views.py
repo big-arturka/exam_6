7ad9da4e6a5002cc818f67c067b314865662e844
Arthur <arturkrmnlv10@gmail.com>
2020-08-01 12:42:58 +0600

    Добавлено представление удаления отзыва, отредактирован html шаблони и форма.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -50,4 +50,9 @@ def entry_update(request, pk):
 
 
 def entry_delete(request, pk):
-    pass
+    entry = get_object_or_404(Entry, pk=pk)
+    if request.method == 'GET':
+        return render(request, 'entry_delete.html', context={'entry': entry})
+    elif request.method == 'POST':
+        entry.delete()
+        return redirect('index')
