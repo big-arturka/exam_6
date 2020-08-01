22fc6cd50a417e63f6ed98135f9cc9da3d3511d4
Arthur <arturkrmnlv10@gmail.com>
2020-08-01 11:28:40 +0600

    Создана модель и проведена миграция.

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+DEFAULT_STATUS = "active"
+ENTRY_STATUS = (
+    (DEFAULT_STATUS, "Активно"),
+    ("active", "Активно"),
+    ("blocked", "Заблокировано"),
+)
+
+
+class Entry(models.Model):
+    author = models.CharField(max_length=100, verbose_name='Автор')
+    email = models.EmailField(max_length=50, verbose_name='Почта')
+    description = models.TextField(max_length=2000, verbose_name='Описание')
+    status = models.CharField(max_length=20, verbose_name='Статус', choices=ENTRY_STATUS, default=DEFAULT_STATUS)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+
+    def __str__(self):
+        return f'{self.author} - {self.status}'
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
