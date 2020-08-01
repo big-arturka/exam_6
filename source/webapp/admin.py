f89365065386cbed2f274ade78a0dad7ec4b8caa
Arthur <arturkrmnlv10@gmail.com>
2020-08-01 11:31:44 +0600

    Модель зарегистрирована в админке.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Entry
 
-# Register your models here.
+
+class EntryAdmin(admin.ModelAdmin):
+    list_filter = ('status',)
+    list_display = ('pk', 'author', 'description', 'status', 'created_at', 'updated_at')
+    list_display_links = ('pk', 'author')
+    search_fields = ('author',)
+
+
+admin.site.register(Entry, EntryAdmin)
