1db6360d087043e638068e992e2389eae48b12d0
Arthur <arturkrmnlv10@gmail.com>
2020-08-01 10:23:53 +0600

    Созданы пустые представления, шаблоны html и form скопированы с прошлого проекта для доработки и редактирования под новый проект. Создан superuser и подключен бутстрап.

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_view(request):
+    pass
+
+
+def product_view(request, pk):
+    pass
+
+
+def product_create(request):
+    pass
+
+
+def product_update(request, pk):
+    pass
+
+
+def product_delete(request, pk):
+    pass
