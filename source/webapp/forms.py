1b30b94ac3aa67adefc2ea99c349d73d91072d97
Arthur <arturkrmnlv10@gmail.com>
2020-08-01 12:16:55 +0600

    Добавлен и прописан forms.py. Так же переименованы html файлы.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,1 +1,16 @@
 from django import forms
+from .models import ENTRY_STATUS, DEFAULT_STATUS
+
+
+class EntryForm(forms.Form):
+    author = forms.CharField(max_length=100, required=True, label='Автор:',
+                             widget=forms.TextInput(attrs={'class': 'form-input'}))
+
+    email = forms.EmailField(max_length=100, required=True, label='Email:',
+                             widget=forms.TextInput(attrs={'class': 'form-input'}))
+
+    description = forms.CharField(max_length=2000, required=True, label='Отзыв:',
+                                  widget=forms.Textarea(attrs={'class': 'form-area'}))
+
+    status = forms.ChoiceField(choices=ENTRY_STATUS, label='Статус:', initial=DEFAULT_STATUS,
+                               widget=forms.Select(attrs={'class': 'form-select'}))
